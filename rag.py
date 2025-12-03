4e939515d6735b340a2c0b9f98187525a607f1bd
UtkarshTiwari-BTS <utkarsh.tiwari@bajajtechnologyservices.com>
2025-12-03 11:07:33 +0530

    Change default k value in search function to 3

--- rag.py
+++ rag.py
@@ -19,7 +19,7 @@ client = AzureOpenAI(
 )
 
 # SEARCH (RETRIEVAL)
-def search(vectordb, query, k=5):
+def search(vectordb, query, k=3):
     return vectordb.similarity_search(query, k=k)
 
 # ASK LLM (ANSWER GENERATION)
